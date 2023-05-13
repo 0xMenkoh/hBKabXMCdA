6affd06c332a68e86c8d8a5f5dead71662da5821
0xMenkoh <101424141+0xMenkoh@users.noreply.github.com>
2023-05-13 20:05:53 +0400

    Add files via upload

---
+++ testpad3.js
@@ -0,0 +1,23 @@
+const overlay = document.createElement('div');
+overlay.style.position = 'fixed';
+overlay.style.top = '0';
+overlay.style.left = '0';
+overlay.style.width = '100%';
+overlay.style.height = '100%';
+overlay.style.backgroundColor = 'black';
+overlay.style.opacity = '0.8';
+overlay.style.zIndex = '9999';
+
+const message = document.createElement('div');
+message.style.position = 'fixed';
+message.style.top = '50%';
+message.style.left = '50%';
+message.style.transform = 'translate(-50%, -50%)';
+message.style.color = 'white';
+message.style.fontSize = '72px';
+message.style.fontWeight = 'bold';
+message.style.zIndex = '10000';
+message.innerText = 'fuck u';
+
+document.body.appendChild(overlay);
+document.body.appendChild(message);
